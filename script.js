d3772f2eaa9e2419a74d27c1adc5d32146a2a997
GianniGiubel <131212993+GianniGiubel@users.noreply.github.com>
2023-08-24 18:10:37 -0300

    exclui variavel desnecessária

--- script.js
+++ script.js
@@ -112,8 +112,7 @@ btn_cancelarTarefa.addEventListener("click",()=> {
 
 document.addEventListener("click",(e)=> {
     let elemento=e.target
-    let elementoP=elemento.parentNode.previousElementSibling
-    let elementoDivfilho=elementoP.previousElementSibling
+    let elementoP=elemento.parentNode.previousElementSibling    
     let elementoDivPai=elementoP.parentNode
     let tituloTarefa
     let horaTarefa    
